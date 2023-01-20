8d322bf57115e02f3400586b65493bb98a361057
Madeesh Kannan <shadeMe@users.noreply.github.com>
2023-01-20 15:06:27 +0100

    Add `Tagger.v3` with softmax temperature parameter (#5)

--- spacy/ml/models/tagger.py
+++ spacy/ml/models/tagger.py
@@ -7,8 +7,10 @@ from ...tokens import Doc
 
 
 @registry.architectures("spacy.Tagger.v2")
-def build_tagger_model(
-    tok2vec: Model[List[Doc], List[Floats2d]], nO: Optional[int] = None, normalize=False
+def build_tagger_model_v2(
+    tok2vec: Model[List[Doc], List[Floats2d]],
+    nO: Optional[int] = None,
+    normalize: bool = False,
 ) -> Model[List[Doc], List[Floats2d]]:
     """Build a tagger model, using a provided token-to-vector component. The tagger
     model simply adds a linear layer with softmax activation to predict scores
@@ -16,11 +18,45 @@ def build_tagger_model(
 
     tok2vec (Model[List[Doc], List[Floats2d]]): The token-to-vector subnetwork.
     nO (int or None): The number of tags to output. Inferred from the data if None.
+    normalize (bool): Normalize the softmax layer outputs.
     """
     # TODO: glorot_uniform_init seems to work a bit better than zero_init here?!
     t2v_width = tok2vec.get_dim("nO") if tok2vec.has_dim("nO") else None
     output_layer = Softmax_v2(
         nO, t2v_width, init_W=zero_init, normalize_outputs=normalize
+    )
+    softmax = with_array(output_layer)  # type: ignore
+    model = chain(tok2vec, softmax)
+    model.set_ref("tok2vec", tok2vec)
+    model.set_ref("softmax", output_layer)
+    model.set_ref("output_layer", output_layer)
+    return model
+
+
+@registry.architectures("spacy.Tagger.v3")
+def build_tagger_model_v3(
+    tok2vec: Model[List[Doc], List[Floats2d]],
+    nO: Optional[int] = None,
+    normalize: bool = False,
+    temperature: float = 1.0,
+) -> Model[List[Doc], List[Floats2d]]:
+    """Build a tagger model, using a provided token-to-vector component. The tagger
+    model simply adds a linear layer with softmax activation to predict scores
+    given the token vectors.
+
+    tok2vec (Model[List[Doc], List[Floats2d]]): The token-to-vector subnetwork.
+    nO (int or None): The number of tags to output. Inferred from the data if None.
+    normalize (bool): Normalize the softmax layer outputs.
+    temperature (float): Temperature used by the softmax layer.
+    """
+    # TODO: glorot_uniform_init seems to work a bit better than zero_init here?!
+    t2v_width = tok2vec.get_dim("nO") if tok2vec.has_dim("nO") else None
+    output_layer = Softmax_v2(
+        nO,
+        t2v_width,
+        init_W=zero_init,
+        normalize_outputs=normalize,
+        temperature=temperature,
     )
     softmax = with_array(output_layer)  # type: ignore
     model = chain(tok2vec, softmax)
